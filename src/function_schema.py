cc5fa4f2a13913c3132c8f338f88b53d2a3f5180
Moha del rey <aslaouisimo1@gmail.com>
2026-09-12 06:12:11 +0100

    Add compact function schema representation

---
+++ src/function_schema.py
@@ -0,0 +1,93 @@
+"""Function schema representation used during constrained generation."""
+
+import json
+from typing import Any
+
+from pydantic import BaseModel, PrivateAttr
+
+from src.token_encoder import Encoder
+
+
+class Function(BaseModel):
+    """Store one function definition and its pre-encoded token data."""
+
+    _name: str = PrivateAttr()
+    _t_name: list[int] = PrivateAttr()
+    _description: str = PrivateAttr()
+    _t_description: list[int] = PrivateAttr()
+    _params: dict[str, str] = PrivateAttr()
+    _t_params: dict[str, list[int]] = PrivateAttr()
+    _t_definition: list[int] = PrivateAttr()
+
+    def __init__(self, function: dict[str, Any], encoder: Encoder):
+        """Build a function object from one JSON definition."""
+        super().__init__()
+        self._name = function['name']
+        self._t_name = encoder.encode(self._name)
+        self._description = function.get('description', '')
+        self._t_description = encoder.encode(self._description)
+        self._params = {
+            name: schema['type']
+            for name, schema in function['parameters'].items()
+        }
+        self._t_params = {
+            name: encoder.encode(schema['type'])
+            for name, schema in function['parameters'].items()
+        }
+        self._t_definition = encoder.encode(self._to_tool_schema())
+
+    def _to_tool_schema(self) -> str:
+        """Return the function definition as a JSON tool schema string."""
+        properties = {
+            name: {'type': param_type}
+            for name, param_type in self._params.items()
+        }
+        return json.dumps({
+            'name': self._name,
+            'description': self._description,
+            'parameters': {
+                'type': 'object',
+                'properties': properties,
+                'required': list(self._params),
+            },
+        })
+
+    @property
+    def t_definition(self) -> list[int]:
+        """Return encoded tool schema tokens."""
+        return self._t_definition
+
+    @property
+    def name(self) -> str:
+        """Return the function name."""
+        return self._name
+
+    @property
+    def t_name(self) -> list[int]:
+        """Return encoded function-name tokens."""
+        return self._t_name
+
+    @property
+    def description(self) -> str:
+        """Return the function description."""
+        return self._description
+
+    @property
+    def t_description(self) -> list[int]:
+        """Return encoded function-description tokens."""
+        return self._t_description
+
+    @property
+    def params(self) -> dict[str, str]:
+        """Return parameter names mapped to their declared types."""
+        return self._params
+
+    @property
+    def t_params(self) -> dict[str, list[int]]:
+        """Return encoded parameter-type tokens."""
+        return self._t_params
+
+    @property
+    def param_names(self) -> list[str]:
+        """Return parameter names in their original declaration order."""
+        return list(self._params)
